65247ea484e08c81c62fd138e4d7f1eb2b21237a
Sobarzo14 <abelardo.sobarzo.14@gmail.com>
2023-03-21 11:40:55 -0400

    Adding my character cards

---
+++ src/card-list.js
@@ -0,0 +1,45 @@
+import { LitElement, html, css } from 'lit';
+import 'special-character-card-2023-ist26-hax-the-web/src/character-card.js';
+import '@lrnwebcomponents/meme-maker/meme-maker.js';
+
+class CardList extends LitElement {
+  static properties = {};
+
+  static styles = css`
+    :host {
+      display: block;
+    }
+  `;
+
+  render() {
+    return html`
+      <character-card></character-card>
+      <character-card
+        name="Minnie Mouse"
+        description="The wifey of Mickey Mouse"
+        image="https://i.ebayimg.com/images/g/uNwAAOSwXyZjUtSc/s-l500.jpg"
+        voice="Russi Taylor"
+      ></character-card>
+      <character-card
+        name="Donald Duck"
+        description="The best friend of Mickey Mouse"
+        image="https://i.ebayimg.com/images/g/Il8AAOSwm9hgkB2p/s-l500.jpg"
+        voice="Tony Anselmo"
+      ></character-card>
+      <character-card
+        name="Goofy"
+        description="The best friend of Mickey Mouse"
+        image="https://i.ebayimg.com/images/g/~RQAAOSwrzdiaZjE/s-l500.jpg"
+        voice="Bill Farmer"
+      ></character-card>
+      <character-card
+        name="Pluto"
+        description="The best friend of Mickey Mouse"
+        image="https://i.ebayimg.com/images/g/3~YAAOSw-45eMXua/s-l500.jpg"
+        voice="Bill Farmer"
+      ></character-card>
+    `;
+  }
+}
+
+customElements.define('card-list', CardList);
